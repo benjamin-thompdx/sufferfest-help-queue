7753aa9062e8737006daf43617a6bd4e5442dcea
Benjamin Thom <jaminthom@gmail.com>
2020-08-06 16:24:34 -0700

    create a functional component called App.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Sufferfest Help Queue</h1>
+      <h3>August 6th</h3>
+      <h3>MPH doesn't seem to be reading correctly</h3>
+      <p><em>When performing my weekly workouts, the MPH reading seems to be way off and I'd like to see accurate stats for all my rides.</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
