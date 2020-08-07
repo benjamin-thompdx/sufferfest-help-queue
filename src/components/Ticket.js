e1f522c730a9e48b22b026acd1db157c5c25b674
Benjamin Thom <jaminthom@gmail.com>
2020-08-06 18:20:03 -0700

    Add another Ticket to the TicketList component

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-function Ticket(){
-  const date = "08/06/2020";
-  const subject = "MPH doesn't seem to be reading correctly";
-  const detail = "When performing my weekly workouts, the MPH reading seems to be way off and I'd like to see accurate stats for all my rides.";
+function Ticket(props){
   return (
     <React.Fragment>
-      <h3>{date}</h3>
-      <h3>{subject}</h3>
-      <p><em>{detail}</em></p>
+      <h3>{props.date}</h3>
+      <h3>{props.subject}</h3>
+      <p><em>{props.detail}</em></p>
       <hr/>
     </React.Fragment>
   );
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -3,11 +3,18 @@ import Ticket from './Ticket';
 
 function TicketList(){
   return (
-    <Ticket 
-      date="08/06/2020"
-      subject="MPH doesn't seem to be reading correctly"
-      detail="When performing my weekly workouts, the MPH reading seems to be way off and I'd like to see accurate stats for all my rides."
-    />
+    <React.Fragment>
+      <Ticket 
+        date="08/01/2020"
+        subject="MPH doesn't seem to be reading correctly"
+        detail="When performing my weekly workouts, the MPH reading seems to be way off and I'd like to see accurate stats for all my rides."
+      />
+      <Ticket
+        date="08/06/2020"
+        subject="My password doesn't work"
+        detail="I keep getting an error when typing my password in on the login page."
+      />
+    </React.Fragment>
   );
 }
 
