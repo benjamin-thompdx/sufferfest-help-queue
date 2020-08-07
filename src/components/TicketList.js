95b40b9908e686ca5237431c96af7dde3585c12b
Benjamin Thom <jaminthom@gmail.com>
2020-08-06 20:52:23 -0700

    update the TicketList component to include a masterTicketList array and use the .map function to loop through the array and create Tickets based on the coresponding props within masterTicketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import Ticket from './Ticket';
 
+const masterTisketList = [
+  {
+    date: "08/01/2020",
+    subject: "MPH doesn't seem to be reading correctly",
+    detail: "When performing my weekly workouts, the MPH reading seems to be way off and I'd like to see accurate stats for all my rides."
+  },
+  {
+    date: "08/06/2020",
+    subject: "My password doesn't work",
+    detail: "I keep getting an error when typing my password in on the login page."
+  },
+  {
+    date: "08/07/2020",
+    subject: "All is working as expected",
+    detail: "No help needed, I just wanted to say thank you for offering such a great product."
+  }
+];
+
 function TicketList(){
   return (
     <React.Fragment>
-      <Ticket 
-        date="08/01/2020"
-        subject="MPH doesn't seem to be reading correctly"
-        detail="When performing my weekly workouts, the MPH reading seems to be way off and I'd like to see accurate stats for all my rides."
-      />
+     <hr/>
+     {masterTisketList.map((ticket, index) =>
       <Ticket
-        date="08/06/2020"
-        subject="My password doesn't work"
-        detail="I keep getting an error when typing my password in on the login page."
+        date={ticket.date}
+        subject={ticket.subject}
+        detail={ticket.detail}
+        key={index}
       />
+     )}
     </React.Fragment>
   );
 }
